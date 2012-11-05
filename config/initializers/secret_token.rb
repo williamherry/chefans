ea1bc6412c9b64542f57db15cda224586e5ffdab
William Herry <WilliamHerryChina@Gmail.com>
2012-11-05 22:47:53 +0800

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chefans::Application.config.secret_token = '9327082a0e40b7020d515b8ccc83473dbf84ee4cb473b9ea4dd1c4f3ebe88e4d041cb9a1a47d4c9f3969c5e26b425c68874292d4c7020082691acb1e6f1db7a9'
